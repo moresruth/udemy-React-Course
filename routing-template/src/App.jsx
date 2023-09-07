b971206efcd13b1ab6ca5c7a6c09acd346b71ad5
Ruth Mores <moresruthantonette@gmail.com>
2023-09-07 22:42:08 +0800

    added query string

--- routing-template/src/App.jsx
+++ routing-template/src/App.jsx
@@ -6,6 +6,8 @@ import Home from "./components/Home/Home";
 import Products from "./components/Products/Products";
 import Articles from "./components/Articles/Articles";
 import Admin from "./components/Admin/Admin";
+import NotFound from "./components/NotFound/NotFound";
+import SingleProduct from "./components/Products/SingleProduct";
 
 const App = () => {
   return (
@@ -16,8 +18,10 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<SingleProduct />} />
           <Route path="/articles" element={<Articles />} />
           <Route path="/admin" element={<Admin />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
     </div>
--- routing-template/src/components/Products/SingleProduct.jsx
+++ routing-template/src/components/Products/SingleProduct.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const SingleProduct = () => {
-    return (
-        <div>
-            <h2>SingleProduct</h2>
-        </div>
-    );
+  const { id } = useParams();
+
+  return (
+    <div>
+      <h2>SingleProduct - {id}</h2>
+    </div>
+  );
 };
 
 export default SingleProduct;
