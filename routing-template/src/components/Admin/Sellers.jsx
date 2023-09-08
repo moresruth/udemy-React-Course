bde1daa8cd3c7fc5e042e23c72feb0ba6fb06422
Ruth Mores <moresruthantonette@gmail.com>
2023-09-08 21:28:12 +0800

    handling errors

--- routing-template/src/components/Admin/Sellers.jsx
+++ routing-template/src/components/Admin/Sellers.jsx
@@ -6,13 +6,20 @@ const Sellers = () => {
   const [name, setName] = useState("");
   const [sellers, setSellers] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [errors, setErrors] = useState("");
 
   useEffect(() => {
     setIsLoading(true);
-    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
-      setSellers(res.data);
-      setIsLoading(false);
-    });
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        setSellers(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setErrors(err.message);
+      });
   }, []);
 
   return (
@@ -20,6 +27,7 @@ const Sellers = () => {
       <h3>Admin Sellers Page</h3>
       <input type="text" onChange={(e) => setName(e.target.value)} />
       {isLoading && <Loader />}
+      {errors && <em>{errors}</em>}
       {sellers.map((seller) => (
         <p key={seller.id}>{seller.name}</p>
       ))}
