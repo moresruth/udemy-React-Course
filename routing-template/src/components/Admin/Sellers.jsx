abfbc8decbb0e1ffbf761e3f623a7e815a6c36ce
Ruth Mores <moresruthantonette@gmail.com>
2023-09-08 21:58:38 +0800

    add newseller

--- routing-template/src/components/Admin/Sellers.jsx
+++ routing-template/src/components/Admin/Sellers.jsx
@@ -9,6 +9,7 @@ const Sellers = () => {
   const [errors, setErrors] = useState("");
 
   useEffect(() => {
+    // fetchSellers();
     setIsLoading(true);
     axios
       .get("https://jsonplaceholder.typicode.com/users")
@@ -22,10 +23,38 @@ const Sellers = () => {
       });
   }, []);
 
+  // const fetchSellers = async () => {
+  //   setIsLoading(true);
+  //   try {
+  //     const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+  //     setSellers(res.data);
+  //     setIsLoading(false);
+  //   } catch (err) {
+  //     setIsLoading(false);
+  //     setErrors(err.message);
+  //   }
+  // };
+
+  const addSeller = () => {
+    const newSeller = {
+      name,
+      id: sellers.length + 1,
+    };
+    setSellers([newSeller, ...sellers]);
+    axios
+      .post("https://jsonplaceholder.typicode.com/users", newSeller)
+      .then((res) => setSellers([res.data, ...sellers]))
+      .catch((err) => {
+        setErrors(err.message);
+        setSellers(sellers);
+      });
+  };
+
   return (
     <>
       <h3>Admin Sellers Page</h3>
       <input type="text" onChange={(e) => setName(e.target.value)} />
+      <button onClick={addSeller}>Add Seller</button>
       {isLoading && <Loader />}
       {errors && <em>{errors}</em>}
       {sellers.map((seller) => (
