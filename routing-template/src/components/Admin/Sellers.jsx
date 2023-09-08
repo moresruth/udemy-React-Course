542a518543f4601eafbc91e320724c4e0a663db0
Ruth Mores <moresruthantonette@gmail.com>
2023-09-08 21:16:38 +0800

    added loading indicator

--- routing-template/src/components/Admin/Sellers.jsx
+++ routing-template/src/components/Admin/Sellers.jsx
@@ -1,20 +1,25 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Loader from "../common/Loader";
 
 const Sellers = () => {
   const [name, setName] = useState("");
   const [sellers, setSellers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setSellers(res.data));
+    setIsLoading(true);
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+      setSellers(res.data);
+      setIsLoading(false);
+    });
   }, []);
 
   return (
     <>
       <h3>Admin Sellers Page</h3>
       <input type="text" onChange={(e) => setName(e.target.value)} />
+      {isLoading && <Loader />}
       {sellers.map((seller) => (
         <p key={seller.id}>{seller.name}</p>
       ))}
