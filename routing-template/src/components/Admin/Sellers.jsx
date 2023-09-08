c52da601236740819b59476fecc278923d38572b
Ruth Mores <moresruthantonette@gmail.com>
2023-09-08 20:56:02 +0800

    fetching data usig axios

--- routing-template/src/components/Admin/Sellers.jsx
+++ routing-template/src/components/Admin/Sellers.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Sellers = () => {
-    return <h3>Admin Sellers Page</h3>;
+  const [name, setName] = useState("");
+  const [sellers, setSellers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setSellers(res.data));
+  }, []);
+
+  return (
+    <>
+      <h3>Admin Sellers Page</h3>
+      <input type="text" onChange={(e) => setName(e.target.value)} />
+      {sellers.map((seller) => (
+        <p key={seller.id}>{seller.name}</p>
+      ))}
+    </>
+  );
 };
 
 export default Sellers;
